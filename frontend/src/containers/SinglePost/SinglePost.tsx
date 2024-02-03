5fc3b6cda142755e660ca6da484cc6fcd4e42940
Susanna Andreeva <sanadex90@gmail.com>
2024-02-03 22:53:38 +0900

    add SinglePost.tsx

--- frontend/src/containers/SinglePost/SinglePost.tsx
+++ frontend/src/containers/SinglePost/SinglePost.tsx
@@ -1,20 +1,20 @@
-import {useParams} from "react-router-dom";
-import PostItem from "../../components/PostItem/PosItem";
-import {useAppSelector} from "../../app/hook";
-import {selectPosts} from "../../store/posts/postsSlise";
+import { useParams } from 'react-router-dom';
+import PostItem from '../../components/PostItem/PosItem';
+import { useAppSelector } from '../../app/hook';
+import { selectPosts } from '../../store/posts/postsSlise';
 
 const SinglePost = () => {
-    const { id } = useParams();
-    const posts = useAppSelector(selectPosts);
-    const post = posts.find(p => p.id === id);
+  const {id} = useParams();
+  const posts = useAppSelector(selectPosts);
+  const post = posts.find(p => p.id === id);
 
-    if (!post) {
-        return <p>Post not found</p>;
-    }
+  if (!post) {
+    return <p>Post not found</p>;
+  }
 
-    return (
-        <PostItem id={post.id} title={post.title} content={post.content} image={post.image} dateTime={post.dateTime} />
-    );
+  return (
+    <PostItem id={post.id} title={post.title} content={post.content} image={post.image} dateTime={post.dateTime}/>
+  );
 };
 
 export default SinglePost;
